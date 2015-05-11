38cb7bf9eaacdfe11768561b50576663eceb0e32
Clement Auger <clement.ager@d1m.cn>
2015-05-11 17:53:21 +0800

    code indent and missing shebang

--- bin.js
+++ bin.js
@@ -1,106 +1,108 @@
-  var pkg = require('./package.json');
-  var fs = require('fs');
-  var through = require('through');
-  var split = require('split');
-  var hostile = require('hostile');
-  var program = require('commander');
+#!/usr/bin/env node
 
-  program.version(pkg.version);
+var pkg = require('./package.json');
+var fs = require('fs');
+var through = require('through');
+var split = require('split');
+var hostile = require('hostile');
+var program = require('commander');
+
+program.version(pkg.version);
 
 
-  var runAsRoot = function(then){
-    if(process.argv.indexOf('--elevated') > -1){
-      then();
-    } else {
-      // when it s windows
-      if(process.platform.match(/win/) && process.arch.match(/32/) ){
-        // ensure the program is not started ad admin yet
-        // then re run node with UAC control for admin privileges
-        var elevate = require('node-windows').elevate;
-        var args = process.argv;
-        args.shift();
-        args.unshift('"'+process.execPath+'"');
-        args.push(' --elevated');
-        elevate(args.join(' '));
-        process.exit(1);
+var runAsRoot = function(then){
+  if(process.argv.indexOf('--elevated') > -1){
+    then();
+  } else {
+    // when it s windows
+    if(process.platform.match(/win/) && process.arch.match(/32/) ){
+      // ensure the program is not started ad admin yet
+      // then re run node with UAC control for admin privileges
+      var elevate = require('node-windows').elevate;
+      var args = process.argv;
+      args.shift();
+      args.unshift('"'+process.execPath+'"');
+      args.push(' --elevated');
+      elevate(args.join(' '));
+      process.exit(1);
+    }else{
+      // otherwise it s linux friendly system
+      // ensure process gid / uid
+      if(require('is-root')()){
+        then();
       }else{
-        // otherwise it s linux friendly system
-        // ensure process gid / uid
-        if(require('is-root')()){
-          then();
-        }else{
-          // otherwise print error
-          console.error('You must be root to run this command:\n');
-          console.error('\tsudo '+process.argv.join(' '));
-          console.error(' ');
-          process.exit(1);
-        }
+        // otherwise print error
+        console.error('You must be root to run this command:\n');
+        console.error('\tsudo '+process.argv.join(' '));
+        console.error(' ');
+        process.exit(1);
       }
     }
+  }
 
-  };
+};
 
-  program.command('load [file]')
-    .option('--elevated', 'Internal option')
-    .description('Load given file into your system')
-    .action(function(file){
-      runAsRoot(function(){
-        file = file || '.hostile';
-        var done = function(){};
-        fs.createReadStream(file, 'utf8')
-          .pipe(split())
-          .pipe(through(online))
-          .on('close', done)
-          .on('error', done);
+program.command('load [file]')
+  .option('--elevated', 'Internal option')
+  .description('Load given file into your system')
+  .action(function(file){
+    runAsRoot(function(){
+      file = file || '.hostile';
+      var done = function(){};
+      fs.createReadStream(file, 'utf8')
+        .pipe(split())
+        .pipe(through(online))
+        .on('close', done)
+        .on('error', done);
 
-        function online (line) {
-          var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line)
-          if (matches && matches.length === 3) {
-            // Found a hosts entry
-            var ip = matches[1];
-            var host = matches[2];
-            hostile.set(ip, host);
-          } else {
-            // Found a comment, blank line, or something else
-            //- ignore it
-          }
+      function online (line) {
+        var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line)
+        if (matches && matches.length === 3) {
+          // Found a hosts entry
+          var ip = matches[1];
+          var host = matches[2];
+          hostile.set(ip, host);
+        } else {
+          // Found a comment, blank line, or something else
+          //- ignore it
         }
-      });
+      }
     });
+  });
 
-  program.command('unload [file]')
-    .option('--elevated', 'Internal option')
-    .description('Unload given file into your system')
-    .action(function(file){
-      runAsRoot(function(){
-        file = file || '.hostile';
-        var done = function(){};
-        fs.createReadStream(file, 'utf8')
-          .pipe(split())
-          .pipe(through(online))
-          .on('close', done)
-          .on('error', done);
+program.command('unload [file]')
+  .option('--elevated', 'Internal option')
+  .description('Unload given file into your system')
+  .action(function(file){
+    runAsRoot(function(){
+      file = file || '.hostile';
+      var done = function(){};
+      fs.createReadStream(file, 'utf8')
+        .pipe(split())
+        .pipe(through(online))
+        .on('close', done)
+        .on('error', done);
 
-        function online (line) {
-          var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line)
-          if (matches && matches.length === 3) {
-            // Found a hosts entry
-            var ip = matches[1];
-            var host = matches[2];
-            hostile.remove(ip, host);
-          } else {
-            // Found a comment, blank line, or something else
-            //- ignore it
-          }
+      function online (line) {
+        var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line)
+        if (matches && matches.length === 3) {
+          // Found a hosts entry
+          var ip = matches[1];
+          var host = matches[2];
+          hostile.remove(ip, host);
+        } else {
+          // Found a comment, blank line, or something else
+          //- ignore it
         }
-      });
+      }
     });
+  });
 
-  program.command('*')
-    .description('Help')
-    .action(function(){
-      program.outputHelp();
-    });
+program.command('*')
+  .description('Help')
+  .action(function(){
+    program.outputHelp();
+  });
 
-  program.parse(process.argv);
+program.parse(process.argv);
 
